9855292895934f16fc349bf8e97eee8cfcc0365a
AadityaPandey30 <aprock3004@gmail.com>
2025-10-28 17:08:28 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-green-500'>React-Vite Tailwind Setup ready</h1>
+    </>
+  )
+}
+
+export default App
